ceb9acb3e12ed95b62c5bb6d6c8efe3183b96cf7
kat.italiano@gmail.com <35376537+katitaliano@users.noreply.github.com>
2023-04-17 22:29:24 +0800

    Update NewProjectForm.jsx

--- src/components/NewProjectForm/NewProjectForm.jsx
+++ src/components/NewProjectForm/NewProjectForm.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import formatCurrency from "../Helpers/FormatCurrency";
 
 function ProjectForm() {
 
@@ -12,57 +11,48 @@ function ProjectForm() {
             description: "",
             target: "",
             image: "",
+            is_open: true,
+            date_created: "",
         }
     )
-    // States for the uploadImage file and formattedTarget currency value:
+    // State for the uploadImage file:
     const [uploadImage, setUploadImage] = useState(null);
-    const [formattedTarget, setFormattedTarget] = useState("");
 
-    // Updates the formattedTarget whenever target changes
-    useEffect(() => {
-        setFormattedTarget(formatCurrency(projectData.target));
-      }, [projectData.target]);      
-   
-    // Handles changes in the title, description, target, and image fields
     // Event handler for form input changes
     const handleChange = (event) => {
+        // Handles changes in the title, description, target, and image fields
         const { name, value, type, checked, files } = event.target;
         if (name === "target" && type === "number") {
-        // Format target amount as currency
-            const formattedValue = new Intl.NumberFormat("en-AU", {
-                style: "currency",
-                currency: "AUD",
-            }).format(value);
+            // Directly set target as integer
             setProjectData((prevProjectData) => ({
                 ...prevProjectData,
-                [name]: formattedValue,
+                [name]: parseInt(value, 10),
+            }));
+        } else if (type === "url") {
+            // Update image link when input value changes
+            setProjectData((prevProjectData) => ({
+                ...prevProjectData,
+                image: value, // Update image property
             }));
         } else if (type === "file" && files.length > 0) {
-        // Update image data when a new file is selected
-        const reader = new FileReader();
-        reader.onload = (e) => {
-            setProjectData((prevProjectData) => ({
-            ...prevProjectData,
-            image: e.target.result,
-            imageLink: "",
-            }));
-        };
-        reader.readAsDataURL(files[0]);
-        } else if (name === "imageLink") {
-        // Update image link when input value changes
-        setProjectData((prevProjectData) => ({
-            ...prevProjectData,
-            image: "",
-            [name]: value,
-        }));
+            // Update image data when a new file is selected
+            const reader = new FileReader();
+            reader.onload = (e) => {
+                setProjectData((prevProjectData) => ({
+                    ...prevProjectData,
+                    image: e.target.result,
+                }));
+            };
+            reader.readAsDataURL(files[0]);
         } else {
-        // Update other form input values
-        setProjectData((prevProjectData) => ({
-            ...prevProjectData,
-            [name]: type === "checkbox" ? checked : value,
-        }));
+            // Update other form input values
+            setProjectData((prevProjectData) => ({
+                ...prevProjectData,
+                [name]: type === "checkbox" ? checked : value,
+            }));
         }
     };
+      
 
     const handleFileUpload = (event) => {
         const { name, files } = event.target;
@@ -72,43 +62,55 @@ function ProjectForm() {
             setUploadImage(() => e.target.result);
         };
         reader.readAsDataURL(files[0]);
-        };
+    };
       
-
-    // Event handler for form submission
     const handleSubmit = async (event) => {
         event.preventDefault();
         const authToken = window.localStorage.getItem("token");
-
+    
         if (authToken) {
-        const formData = new FormData();
-        formData.append("title", projectData.title);
-        formData.append("description", projectData.description);
-        formData.append("target", projectData.target);
-        formData.append("image", uploadImage);
-
-        // Send form data to API endpoint for project creation
-        const response = await fetch(`${import.meta.env.VITE_API_URL}projects/`, {
-            method: "POST",
-            headers: {
-            Authorization: `Token ${authToken}`,
-            },
-            body: formData,
-        });
-
-        if (response.ok) {
-            const responseData = await response.json();
-            console.log("Success:", responseData);
-        } else {
-            console.log("Error:", response.statusText);
+            const formData = new FormData();
+            formData.append("title", projectData.title);
+            formData.append("description", projectData.description);
+            formData.append('target', projectData.target);
+    
+            // Get current date and time
+            const currentDate = new Date();
+            formData.append('date_created', currentDate.toISOString());
+    
+            // Append image link as a string to formData
+            if (uploadImage) {
+                formData.append("image", uploadImage);
+            } else if (projectData.image) {
+                formData.append("image", projectData.image);
+            }
+    
+            const response = await fetch(`${import.meta.env.VITE_API_URL}projects/`, {
+                method: "POST",
+                headers: {
+                    Authorization: `Token ${authToken}`,
+                },
+                body: formData,
+            });
+    
+            if (response.ok) {
+                // Parse the response JSON
+                const data = await response.json();
+    
+                // Extract the `id` from the response
+                const id = data.id;
+    
+                // Navigate to the project page with the extracted `id`
+                navigate(`/project/${id}`);
+            } else {
+                console.log("Error:", response.statusText);
+            }
+            console.log(formData);
         }
-        } else {
-        // Redirect to login page if user is not authenticated
-        navigate(`/login`);
-        }
-        console.log(projectData);
-    };
-  
+    }
+    
+    
+    
     return (
         <form onSubmit={handleSubmit}>
             <div className="form-item">
@@ -127,7 +129,7 @@ function ProjectForm() {
                 <label htmlFor="image">Image: </label>
                 <input type="url" id="image" name="image" onChange={handleChange} placeholder="Insert image link" />
                 <p>OR</p>
-                <input type="file" id="image" name="image" onChange={handleFileUpload} />
+                <input type="url" id="image" name="image" onChange={handleFileUpload} />
             </div>
             <div className="form-item">
                 <button type="Submit">Post Project!</button>
@@ -136,4 +138,4 @@ function ProjectForm() {
     )
 }
 
-export default ProjectForm
+export default ProjectForm;
